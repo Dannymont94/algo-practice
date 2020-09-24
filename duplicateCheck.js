6edf11ea2e4c50b4994670bc773cf1f2ee936ab5
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-24 19:01:41 -0400

    wrote and benchmarked optimized duplicate check function

--- duplicateCheck.js
+++ duplicateCheck.js
@@ -33,16 +33,56 @@ function nestedLoopDuplicateCheck(arr) {
   return `${mostValue} appeared ${mostCount} times.`;
 }
 
+function optimizedDuplicates(arr) {
+  // create empty object map
+  const map = {};
+
+  // else, increment key/value pair by 1
+  
+  // loop over array
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i];
+    
+    // if array[i] not in map, create new key/value pair on map = 1
+    if (map[value] === undefined) {
+      map[value] = 1;
+    } else {
+      map[value]++;
+    }
+  }
+  
+  // create variable for most appearing value
+  let mostValue;
+  
+  // create variable for most appearing count
+  let mostCount = 0;
+  
+  // loop over object keys/values
+  for (const value in map) {
+    // if value > current most appearing count, update "most" variables to match
+    if (map[value] > mostCount) {
+      mostCount = map[value];
+      mostValue = value;
+    }
+  }
+    
+  // return most appearing value and count
+  return `${mostValue} appeared ${mostCount} times.`;
+}
+
 const numbers = [];
-for (let i = 0; i < 40000; i++) {
+for (let i = 0; i < 10000; i++) {
   numbers.push(Math.floor(Math.random() * 10000) + 1);
 }
 
 const suite = new Benchmark.Suite;
 
 suite
-  .add('duplicates test', function() {
+  .add('nested loops duplicates test', function() {
     nestedLoopDuplicateCheck(numbers);
+  })
+  .add('object key duplicates test', function() {
+    optimizedDuplicates(numbers);
   })
   .on('complete', function() {
     this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean * 1000} milliseconds`));
